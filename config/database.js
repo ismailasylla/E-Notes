fc7d0874b15cd948cc1fb03f395e92b6fffd67fc
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2019-01-23 20:48:16 +0530

    adding security to the app

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = { mongoURI: 'mongodb://isma:American1@ds229465.mlab.com:29465/e-notes' }
+  module.exports = {
+    mongoURI: 'mongodb://isma:Americangoogle1@ds111065.mlab.com:11065/e-note'
+  };
 } else {
-    module.exports = { mongoURI: 'mongodb://localhost/E-notes' }
+  module.exports = { mongoURI: 'mongodb://localhost/E-notes' };
 }
