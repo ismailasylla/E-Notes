383aec0e73770eae494f32eb5fb98955d65327d2
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2019-01-23 20:52:42 +0530

    ...

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: 'mongodb://isma:Americangoogle1@ds111065.mlab.com:11065/e-note'
+    mongoURI: 'mongodb://isma:enote12345@ds111065.mlab.com:11065/e-note'
   };
 } else {
   module.exports = { mongoURI: 'mongodb://localhost/E-notes' };
