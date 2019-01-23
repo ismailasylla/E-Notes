6c51297487c637a52efdb595af8a45d199fce9d3
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2019-01-23 21:03:49 +0530

    preparing for depaloy

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: 'mongodb://isma:enote12345@ds111065.mlab.com:11065/e-note'
+    mongoURI: 'mongodb://isma:enote12345@ds163984.mlab.com:63984/e-note'
   };
 } else {
   module.exports = { mongoURI: 'mongodb://localhost/E-notes' };
