af8531c5b509309f0b00247051083e75933ceb5c
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2019-01-24 16:34:10 +0530

    ...

--- app.js
+++ app.js
@@ -22,16 +22,23 @@ const db = require('./config/database');
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
 // Connect to mongoose
-mongoose.connect(db.mongoURI, {
-        useMongoClient: true
-    })
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+mongoose
+  .connect(
+    db.mongoURI,
+    {
+      useMongoClient: true
+    }
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Handlebars Middleware
-app.engine('handlebars', exphbs({
+app.engine(
+  'handlebars',
+  exphbs({
     defaultLayout: 'main'
-}));
+  })
+);
 app.set('view engine', 'handlebars');
 
 // Body parser middleware
@@ -45,11 +52,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 // Express session midleware
-app.use(session({
+app.use(
+  session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
-}));
+  })
+);
 
 // Passport middleware
 app.use(passport.initialize());
@@ -59,26 +68,25 @@ app.use(flash());
 
 // Global variables
 app.use(function(req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    res.locals.user = req.user || null;
-    next();
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
 
 // Index Route
 app.get('/', (req, res) => {
-    const title = 'Welcome';
-    res.render('index', {
-        title: title
-    });
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
 });
 
 // About Route
 app.get('/about', (req, res) => {
-    res.render('about');
+  res.render('about');
 });
-
 
 // Use routes
 app.use('/ideas', ideas);
@@ -87,5 +95,5 @@ app.use('/users', users);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
--- models/idea.js
+++ models/idea.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//Create Schema
-
+// Create Schema
 const IdeaSchema = new Schema({
   title: {
     type: String,
