f36598a5a21bd034aa1542d239e72a1162a6a052
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2019-01-24 13:32:12 +0530

    fixing logs

--- models/idea.js
+++ models/idea.js
@@ -4,23 +4,22 @@ const Schema = mongoose.Schema;
 //Create Schema
 
 const IdeaSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    details: {
-        type: String,
-        required: true
-    },
-    user: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-
+  title: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 mongoose.model('ideas', IdeaSchema);
--- routes/ideas.js
+++ routes/ideas.js
@@ -9,95 +9,88 @@ const Idea = mongoose.model('ideas');
 
 // Idea Index Page
 router.get('/', ensureAuthenticated, (req, res) => {
-    Idea.find({ user: req.user.id })
-        .sort({ date: 'desc' })
-        .then(ideas => {
-            res.render('ideas/index', {
-                ideas: ideas
-            });
-        });
+  Idea.find({ user: req.user.id })
+    .sort({ date: 'desc' })
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas: ideas
+      });
+    });
 });
 
 // Add Idea Form
 router.get('/add', ensureAuthenticated, (req, res) => {
-    res.render('ideas/add');
+  res.render('ideas/add');
 });
 
 // Edit Idea Form
 router.get('/edit/:id', ensureAuthenticated, (req, res) => {
-    Idea.findOne({
-            _id: req.params.id
-        })
-        .then(idea => {
-            if (idea.user != req.user.id) {
-                req.flash('error_msg', 'Not Authorized');
-                res.redirect('/ideas');
-            } else {
-                res.render('ideas/edit', {
-                    idea: idea
-                });
-            }
-
-        });
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    if (idea.user != req.user.id) {
+      req.flash('error_msg', 'Not Authorized');
+      res.redirect('/ideas');
+    } else {
+      res.render('ideas/edit', {
+        idea: idea
+      });
+    }
+  });
 });
 
 // Process Form
 router.post('/', ensureAuthenticated, (req, res) => {
-    let errors = [];
+  let errors = [];
 
-    if (!req.body.title) {
-        errors.push({ text: 'Please add a title' });
-    }
-    if (!req.body.details) {
-        errors.push({ text: 'Please add some details' });
-    }
+  if (!req.body.title) {
+    errors.push({ text: 'Please add a title' });
+  }
+  if (!req.body.details) {
+    errors.push({ text: 'Please add some details' });
+  }
 
-    if (errors.length > 0) {
-        res.render('/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-    } else {
-        const newUser = {
-            title: req.body.title,
-            details: req.body.details,
-            user: req.user.id
-        }
-        new Idea(newUser)
-            .save()
-            .then(idea => {
-                req.flash('success_msg', 'Note added');
-                res.redirect('/ideas');
-            })
-    }
+  if (errors.length > 0) {
+    res.render('/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details,
+      user: req.user.id
+    };
+    new Idea(newUser).save().then(idea => {
+      req.flash('success_msg', 'Note added');
+      res.redirect('/ideas');
+    });
+  }
 });
 
 // Edit Form process
 router.put('/:id', ensureAuthenticated, (req, res) => {
-    Idea.findOne({
-            _id: req.params.id
-        })
-        .then(idea => {
-            // new values
-            idea.title = req.body.title;
-            idea.details = req.body.details;
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    // new values
+    idea.title = req.body.title;
+    idea.details = req.body.details;
 
-            idea.save()
-                .then(idea => {
-                    req.flash('success_msg', 'Note updated');
-                    res.redirect('/ideas');
-                })
-        });
+    idea.save().then(idea => {
+      req.flash('success_msg', 'Note updated');
+      res.redirect('/ideas');
+    });
+  });
 });
 
 // Delete Idea
 router.delete('/:id', ensureAuthenticated, (req, res) => {
-    Idea.remove({ _id: req.params.id })
-        .then(() => {
-            req.flash('success_msg', 'Note removed');
-            res.redirect('/ideas');
-        });
+  Idea.remove({ _id: req.params.id }).then(() => {
+    req.flash('success_msg', 'Note removed');
+    res.redirect('/ideas');
+  });
 });
 
 module.exports = router;
